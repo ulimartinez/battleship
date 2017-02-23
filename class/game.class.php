34443a97e10f0c556e70c33101296d428d419d8c
Ulises <ulimartinez96@gmail.com>
2017-02-22 21:21:52 -0700

    implemented autoloading

--- class/game.class.php
+++ class/game.class.php
@@ -1,8 +1,7 @@
 <?php
-require_once 'shipPlacement.class.php';
-require_once 'ship.class.php';
-require_once 'board.class.php';
-require_once 'strategy.class.php';
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.class.php';
+});
 class Game{
   public $board;
   public $boardpc;
@@ -19,6 +18,7 @@ class Game{
     $this->shipPlacementspc = $this->create_ships();
   }
   public static function createFromJson($json_str){
+    //TODO: check if pc ship placement works correctly
     $game = json_decode($json_str);
     //create the object
     $tmp_game = new self();
@@ -33,7 +33,7 @@ class Game{
       }
     }
     foreach ($game->shipPlacementspc as $tmp_placement) {
-      $tmp_ship = $tmp_game->ship_exists($tmp_placement->ship->name);
+      $tmp_ship = $tmp_game->ship_exists($tmp_placement->ship->name, true);
       if($tmp_ship){
         $tmp_ship->setCoordinate($tmp_placement->xcoordinate, $tmp_placement->ycoordinate);
         $tmp_ship->setIsHorizontal($tmp_placement->isHorizontal);
@@ -76,8 +76,12 @@ class Game{
     }
     return true;
   }
-  protected function ship_exists($ship_name){
-    foreach($this->shipPlacements as $shipPlacement){
+  protected function ship_exists($ship_name, $isPc = false){
+    $placements = $this->shipPlacements;
+    if($isPc){
+      $placements = $this->shipPlacementspc;
+    }
+    foreach($placements as $shipPlacement){
       if(strcasecmp($ship_name, $shipPlacement->getShip()->getName()) == 0){
         return $shipPlacement;
       }
@@ -156,15 +160,10 @@ class Game{
 
   function set_strategy($currentStrategy){
     $this->currentStrategy = $currentStrategy;
-      $statergie=new Strategy($currentStrategy);
-      if($statergie->getStrategy() == "Smart"){
-        $statergie->smartStrategy();
-      } elseif ($statergie->getStrategy() == "Random") {
-        $statergie->randomStrategy();
-      } else {
-        $statergie->sweepStrategy();
-      }
-    }
+  }
+  function getStrategy(){
+    return $this->currentStrategy;
+  }
 
 }
 ?>
--- class/strategy.class.php
+++ class/strategy.class.php
@@ -1,21 +1,6 @@
 <?php
-class Strategy{
-  public $strategy;
-
-  function __construct($strategy){
-    $this->strategy = $strategy;
-  }
-  function getStrategy(){
-    return $this->strategy;
-  }
-  function smartStrategy(){
-    //create a smartstraegy
-  }
-  function randomStrategy(){
-  //create a smartstraegy
-  }
-  function sweepStrategy(){
-    //create a smartstraegy
-  }
+namespace Shots;
+interface Strategy{
+  function shoot();
 }
 ?>
